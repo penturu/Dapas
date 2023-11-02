1b91164efa5ed202179238b8da575eb693ece26a
Anugerah Ashar <hutanalaska382@gmail.com>
2023-11-02 13:48:36 +0800

    file koneksi (jangan lupa edit passwordnya, jika perlu)

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php 
+
+$databaseHost = 'localhost';
+$databaseName = 'rekmed';
+$databaseUsername = 'root';
+$databasePassword = '';
+$con = mysqli_connect(
+ "$databaseHost",
+ "$databaseUsername",
+ "$databasePassword",
+ "$databaseName"
+);
+if (mysqli_connect_errno()) {
+ echo '<h1>Koneksi database error : ' . mysqli_connect_errno() . '</h1>';
+}
+
+?>
